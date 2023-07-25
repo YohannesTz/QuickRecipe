0f146a80a5bbf509070bd19eb7198fcfbf5665be
yohannesTz <yohannes222ethiopia@gmail.com>
2023-07-26 00:42:49 +0300

    removed exclude

--- src/main/java/com/github/yohannestz/QuickRecipe/QuickRecipeApplication.java
+++ src/main/java/com/github/yohannestz/QuickRecipe/QuickRecipeApplication.java
@@ -1,7 +1,6 @@
 package com.github.yohannestz.QuickRecipe;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
