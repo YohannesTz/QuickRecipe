81b978cc4261aa27205d6f3232f1d6b6b064d0a9
yohannesTz <yohannes222ethiopia@gmail.com>
2023-07-26 00:36:06 +0300

    removed exclude

--- src/main/java/com/github/yohannestz/QuickRecipe/QuickRecipeApplication.java
+++ src/main/java/com/github/yohannestz/QuickRecipe/QuickRecipeApplication.java
@@ -5,11 +5,7 @@ import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoC
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(
-		exclude = {
-				SystemMetricsAutoConfiguration.class
-		}
-)
+@SpringBootApplication()
 @EntityScan("com.github.yohannestz.QuickRecipe.domain")
 public class QuickRecipeApplication {
 	public static void main(String[] args) {
